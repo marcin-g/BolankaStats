8c74b38ca654e28f766d63b72152055c3ca719da
Marcin Grzeskowiak <marcin.grzeskowiak@hotmail.com>
2016-02-16 00:08:35 +0100

    Common helper with device id

---
+++ BolankaStats/BolankaStats.Shared/Common/CommonHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Windows.System.Profile;
+
+namespace BolankaStats.Common
+{
+    static class CommonHelper
+    {
+        public static string DEVICE_ID;
+        static CommonHelper()
+        {
+            HardwareToken token = HardwareIdentification.GetPackageSpecificToken(null);
+            IBuffer hardwareId = token.Id;
+
+            HashAlgorithmProvider hasher = HashAlgorithmProvider.OpenAlgorithm("MD5");
+            IBuffer hashed = hasher.HashData(hardwareId);
+
+            string hashedString = CryptographicBuffer.EncodeToHexString(hashed);
+            DEVICE_ID=hashedString;
+        }
+  
+    }
+}
