9b597b826617a4baa7070a8fa1d504a638f187e2
Marcin Grzeskowiak <marcin.grzeskowiak@hotmail.com>
2016-03-01 00:47:14 +0100

    Component tree search

--- BolankaStats/BolankaStats.Shared/Common/CommonHelper.cs
+++ BolankaStats/BolankaStats.Shared/Common/CommonHelper.cs
@@ -5,6 +5,8 @@ using Windows.Security.Cryptography;
 using Windows.Security.Cryptography.Core;
 using Windows.Storage.Streams;
 using Windows.System.Profile;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 
 namespace BolankaStats.Common
 {
@@ -22,6 +24,31 @@ namespace BolankaStats.Common
             string hashedString = CryptographicBuffer.EncodeToHexString(hashed);
             DEVICE_ID=hashedString;
         }
-  
+        public static DependencyObject FindChildControl<T>(DependencyObject control, string ctrlName)
+        {
+            int childNumber = VisualTreeHelper.GetChildrenCount(control);
+            for (int i = 0; i < childNumber; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(control, i);
+                FrameworkElement fe = child as FrameworkElement;
+                // Not a framework element or is null
+                if (fe == null) return null;
+
+                if (child is T && fe.Name == ctrlName)
+                {
+                    // Found the control so return
+                    return child;
+                }
+                else
+                {
+                    // Not found it - search children
+                    DependencyObject nextLevel = FindChildControl<T>(child, ctrlName);
+                    if (nextLevel != null)
+                        return nextLevel;
+                }
+            }
+            return null;
+        }
+
     }
 }
